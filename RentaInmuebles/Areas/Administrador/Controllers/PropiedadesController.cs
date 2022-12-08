f1d890f941bef06ce162c1b76f12430fc1fd8f71
191G0235@rcarbonifera.tecnm.mx <191G0235@rcarbonifera.tecnm.mx>
2022-12-08 02:25:38 -0600

    Crud casi completo de propiedad

--- RentaInmuebles/Areas/Administrador/Controllers/PropiedadesController.cs
+++ RentaInmuebles/Areas/Administrador/Controllers/PropiedadesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using RentaInmuebles.Areas.Administrador.Models.ViewModels;
 using RentaInmuebles.Models;
 using RentaInmuebles.Models.ViewModels;
 
@@ -9,16 +10,18 @@ namespace RentaInmuebles.Areas.Administrador.Controllers
     public class PropiedadesController : Controller
     {
         private readonly sistem21_inmueblesContext context;
+        private readonly IWebHostEnvironment env;
 
-        public PropiedadesController(sistem21_inmueblesContext context)
+        public PropiedadesController(sistem21_inmueblesContext context, IWebHostEnvironment env)
         {
             this.context = context;
+            this.env = env;
         }
 
         [Route("/Administrador/Propiedades")]
         public IActionResult Index()
         {
-            var propiedades = context.Propiedad.Where(x => x.Disponible == Dispinible.Si).OrderByDescending(x => x.Precio).ToList();
+            var propiedades = context.Propiedad.OrderByDescending(x => x.Precio).ToList();
             var ciudades = context.Ciudad.OrderBy(x => x.Nombre).ToList();
 
             if (propiedades == null)
@@ -50,11 +53,11 @@ namespace RentaInmuebles.Areas.Administrador.Controllers
 
             if (vm.IdCiudad == 0)//No eligieron categoria, selecciono todo
             {
-                vm.Propiedades = context.Propiedad.Where(x => x.Disponible == Dispinible.Si).OrderByDescending(x => x.Precio).ToList();
+                vm.Propiedades = context.Propiedad.OrderByDescending(x => x.Precio).ToList();
             }
             else
             {
-                vm.Propiedades = context.Propiedad.Where(x => x.Idciudad == vm.IdCiudad && x.Disponible == Dispinible.Si).Include(x => x.IdciudadNavigation).OrderBy(x => x.Nombre).ToList();
+                vm.Propiedades = context.Propiedad.Where(x => x.Idciudad == vm.IdCiudad).Include(x => x.IdciudadNavigation).OrderBy(x => x.Nombre).ToList();
             }
 
             vm.Ciudades = context.Ciudad.OrderBy(x => x.Nombre).ToList();
@@ -62,7 +65,161 @@ namespace RentaInmuebles.Areas.Administrador.Controllers
             return View(vm);
         }
 
+        public IActionResult Agregar()
+        {
+            AgregarPropiedadViewModel vm = new()
+            {
+                Ciudades = context.Ciudad.OrderBy(x => x.Nombre).ToList()
+            };
 
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Agregar(AgregarPropiedadViewModel vm)
+        {
+            var propiedad = vm.Propiedad;
+            var imagen = vm.Imagen;
+
+            if (propiedad == null)
+                ModelState.AddModelError("", "Por favor escriba los datos correspondientes para continuar");
+
+            if (string.IsNullOrWhiteSpace(propiedad.Nombre))
+                ModelState.AddModelError("", "Por favor escriba el nombre para agregar una propiedad");
+
+            if (string.IsNullOrWhiteSpace(propiedad.Direccion))
+                ModelState.AddModelError("", "Por favor escriba la direccion para agregar una propiedad");
+
+            if (propiedad.Precio <= 0)
+                ModelState.AddModelError("", "Monto invalido. Para agregar una propiedad debe escribir una monto mayor a $0.00");
+
+            if(propiedad.CantCuartos<=0)
+                ModelState.AddModelError("", "NÚmero de cuartos invalido. Para agregar una propiedad debe escribir un número de cuartos mayor a 0");
+
+            if(propiedad.Idciudad==0)
+                ModelState.AddModelError("", "Ciudad invalida. Debe seleccionar la ciudad de la propiedad");
+
+            if(context.Propiedad.Any(x=>x.Direccion==propiedad.Direccion && x.Idciudad == propiedad.Idciudad))
+                ModelState.AddModelError("", "Ya existe una propiedad con esa dirección.");
+
+            if (ModelState.IsValid)
+            {
+                Propiedad p = new();
+                p = propiedad;
+                context.Add(p);
+               int cambios =  context.SaveChanges();
+
+                if (cambios > 0)
+                {
+                    if (vm.Imagen == null)
+                    {
+                        string nodisp = env.WebRootPath + "/img/no-disponible.jpg";
+                        string nuevaruta = env.WebRootPath + $"/img/{p.Id}.jpg";
+
+                        System.IO.File.Copy(nodisp, nuevaruta);
+                    }
+                    else
+                    {
+                        string nuevaruta = env.WebRootPath + $"/img/{p.Id}.jpg";
+                        var archivo = System.IO.File.Create(nuevaruta);
+                        vm.Imagen.CopyTo(archivo);
+                        archivo.Close();
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                vm.Ciudades = context.Ciudad.OrderBy(x => x.Nombre).ToList();
+                return View(vm);
+            }
+            
+        }
+
+
+        public IActionResult Editar(int id)
+        {
+            var p = context.Propiedad.Find(id);
+
+            if (p == null)
+                RedirectToAction("Index");
+
+            AgregarPropiedadViewModel vm = new()
+            {
+                Ciudades = context.Ciudad.OrderBy(x => x.Nombre).ToList(),
+                Propiedad=p
+               
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(AgregarPropiedadViewModel vm)
+        {
+            var propiedad = vm.Propiedad;
+            var imagen = vm.Imagen;
+
+            if (vm.Propiedad == null)
+                return RedirectToAction("Index");
+
+            var p = context.Propiedad.Find(propiedad.Id);
+
+            if(p == null)
+                return RedirectToAction("Index");
+
+            if (propiedad == null)
+                ModelState.AddModelError("", "Por favor escriba los datos correspondientes para continuar");
+
+            if (string.IsNullOrWhiteSpace(propiedad.Nombre))
+                ModelState.AddModelError("", "Por favor escriba el nombre para agregar una propiedad");
+
+            if (string.IsNullOrWhiteSpace(propiedad.Direccion))
+                ModelState.AddModelError("", "Por favor escriba la direccion para agregar una propiedad");
+
+            if (propiedad.Precio <= 0)
+                ModelState.AddModelError("", "Monto invalido. Para agregar una propiedad debe escribir una monto mayor a $0.00");
+
+            if (propiedad.CantCuartos <= 0)
+                ModelState.AddModelError("", "NÚmero de cuartos invalido. Para agregar una propiedad debe escribir un número de cuartos mayor a 0");
+
+            if (propiedad.Idciudad == 0)
+                ModelState.AddModelError("", "Ciudad invalida. Debe seleccionar la ciudad de la propiedad");
+
+            if (context.Propiedad.Any(x => x.Direccion == propiedad.Direccion && x.Idciudad == propiedad.Idciudad &&x.Id !=p.Id))
+                ModelState.AddModelError("", "Ya existe una propiedad con esa dirección.");
+
+            if (ModelState.IsValid)
+            {
+                
+                p.Nombre = propiedad.Nombre;
+                p.Direccion = propiedad.Direccion;
+                p.Precio = propiedad.Precio;
+                p.CantBaños = propiedad.CantBaños;
+                p.CantCochera = propiedad.CantCochera;
+                p.CantCuartos = propiedad.CantCuartos;
+                p.Idciudad = propiedad.Idciudad;
+                p.Descripcion = propiedad.Descripcion;
+                p.Disponible = propiedad.Disponible;
+                context.SaveChanges();
+
+                if (vm.Imagen != null)
+                {
+                    string nuevaruta = env.WebRootPath + $"/img/{p.Id}.jpg";
+                    var archivo = System.IO.File.Create(nuevaruta);
+                    vm.Imagen.CopyTo(archivo);
+                    archivo.Close();
+                }
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                vm.Ciudades = context.Ciudad.OrderBy(x => x.Nombre);
+                return View(vm);
+            }
+        }
 
     }
 }
---
+++ RentaInmuebles/Areas/Administrador/Models/ViewModels/AgregarPropiedadViewModel.cs
@@ -0,0 +1,11 @@
+﻿using RentaInmuebles.Models;
+
+namespace RentaInmuebles.Areas.Administrador.Models.ViewModels
+{
+    public class AgregarPropiedadViewModel
+    {
+        public Propiedad? Propiedad { get; set; } = new();
+        public IEnumerable<Ciudad>? Ciudades { get; set; }
+        public IFormFile? Imagen { get; set; }
+    }
+}
