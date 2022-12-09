bda8490fb0e6d660a6cba2b7592d35e9215768c9
191G0235@rcarbonifera.tecnm.mx <191G0235@rcarbonifera.tecnm.mx>
2022-12-08 23:25:10 -0600

    Ruteo en inicio de sesion

--- RentaInmuebles/Controllers/HomeController.cs
+++ RentaInmuebles/Controllers/HomeController.cs
@@ -136,7 +136,7 @@ namespace RentaInmuebles.Controllers
                 return View(propiedadViewModel);
         }
 
-
+        [Route("/IniciarSesion")]
         public IActionResult IniciarSesion()
         {
             ViewBag.IniciarSesion = true;
